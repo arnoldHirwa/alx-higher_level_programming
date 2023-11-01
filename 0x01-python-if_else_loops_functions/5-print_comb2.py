08701513bb7899ab6580fbf5d08d6b700d6b09fc
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-01 03:37:40 +0200

    try again

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print(f'0{i}', end=", ")
+        print('0{}'.format(i), end=", ")
     else:
         if i == 99:
             print(i)
