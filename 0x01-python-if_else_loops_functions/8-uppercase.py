7e6cc570fcafab710d87d79e356f12a9552155d5
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-01 04:39:06 +0200

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        i = str[i]
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+        print('{}'.format(i), end='')
+    print()
