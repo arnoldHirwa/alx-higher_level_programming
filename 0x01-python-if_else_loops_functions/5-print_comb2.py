fa7e63bbe1362b468d458918dc1daefe12aabc03
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-01 03:41:30 +0200

    Fix error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print('0{}'.format(i), end=", ")
+        i = '0' + str(i)
+    if i == 99:
+        print('{}'.format(i))
     else:
-        if i == 99:
-            print(i)
-        else:
-            print(i, end=", ")
+        print('{}'.format(i), end=", ")
