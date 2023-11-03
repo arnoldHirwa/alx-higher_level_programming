84c7231dc79c8b8a6ef8d5376553eab9bf7a64de
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-03 03:57:43 +0200

    Use CLI arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        if len(argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(argv)))
+        for n in range(1, len(argv)):
+            print("{}: {}".format(n, argv[n]))
