542e57888b96680fbbb12f9d6a0a7f2a0a2e6a1b
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-03 04:00:43 +0200

    Fix: len of argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         if len(argv) == 2:
             print("1 argument:")
         else:
-            print("{} arguments:".format(len(argv)))
+            print("{} arguments:".format(len(argv) - 1))
         for n in range(1, len(argv)):
             print("{}: {}".format(n, argv[n]))
