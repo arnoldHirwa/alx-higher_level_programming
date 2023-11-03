46b4e663807907bc4dde6916d8f0ec227dfea0a6
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-03 03:40:44 +0200

    Use module functions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import calculator_1 as calc
+
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
