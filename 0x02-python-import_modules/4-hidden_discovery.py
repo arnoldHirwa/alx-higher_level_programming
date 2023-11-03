5fc2af900a2041e6febd2662aedac5e2e7291e94
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-03 04:24:18 +0200

    Print functions from executable file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import importlib.util as utils
+
+if __name__ == "__main__":
+    path = "hidden_4.pyc"
+    spec = utils.spec_from_file_location("hidden_4", path)
+    myMod = utils.module_from_spec(spec)
+    spec.loader.exec_module(myMod)
+    # Get names from module
+    for name in (dir(myMod)):
+        if not name.startswith('__'):
+            print(name)
