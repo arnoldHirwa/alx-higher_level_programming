4ef75b6072ab670878630f1893f59c7a7d1779e0
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-07 01:52:45 +0200

    Fix error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    new = []
-    for i in range(len(my_list)):
-        new[i] = my_list[i]
+    new = my_list.copy()  # Create a copy of my_list to start with
+
     if (idx < 0 or idx > (len(my_list) - 1)):
         return new
     else:
