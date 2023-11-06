9b5ec48d83cf0b8c233303cafe2e3b570212d500
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-07 01:37:08 +0200

    Fix error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(len(my_list) - i - 1))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[len(my_list) - i - 1]))
