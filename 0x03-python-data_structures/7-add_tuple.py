5339fcdf34b8684b33967c3bc34dbd50e8cc60b2
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-07 02:48:50 +0200

    Add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        a, b = (0, 0)
+    elif len(tuple_a) == 1:
+        a = tuple_a[0]
+        b = 0
+    else:
+        a = tuple_a[0]
+        b = tuple_a[1]
+    if len(tuple_b) == 0:
+        c, d = (0, 0)
+    elif len(tuple_b) == 1:
+        c = tuple_a[0]
+        d = 0
+    else:
+        c = tuple_b[0]
+        d = tuple_b[1]
+    new_tuple = (a + c, b + d)
+    return(new_tuple)
+
