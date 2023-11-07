e2ba94b21cd83bfafc9c89f75c61b93fd66da3ad
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-07 05:01:53 +0200

    Another aproach

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,56 +1,33 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * checkPal - check if a list is palindrome
- * @array: pointer to list
- * @index_a: primero indice (inicial)
- * @index_b: seg indice (final)
- * Return: return 1 if list is palindrome or 0 otherwise
- */
-int checkPal(int *array, int index_a, int index_b)
-{
-	if (index_a > index_b)
-		return (1);
-	if (array[index_a] != array[index_b])
-	{
-		return (0);
-	}
-	return (checkPal(array, index_a + 1, index_b - 1));
-}
-/**
- * is_palindrome - check if a single linked list is palindrome
- * @head: pointer to node listint_t
- * Return: return 1 if list is palindrome or 0 otherwise
+ * is_palindrome - test if is a palindrome
+ * @head: firts node of list
+ * Return: 1 if is palindrome, 0 for no
  */
 int is_palindrome(listint_t **head)
 {
-	int len = 0;
-	int i = 0; /*, j = 0, node_a = 0, node_b = 0, index_a = 0, index_b = 0;*/
-	const listint_t *temp = *head;
-	int *array;
+	listint_t *buscar;
+	int con1 = 0, con2 = 0;
+	int arr[4096];
 
-	if (!*head)
-		return (1);
-	while (temp)
-	{
-		temp = temp->next;
-		len++;
-	}
-	temp = *head;
-	array = malloc(sizeof(int) * len);
-	if (!array)
+
+	if (!head)
 		return (0);
-	for (i = 0; i <= (len - 1); i++)
-	{
-		array[i] = temp->n;
-		temp = temp->next;
-	}
-	if (checkPal(array, 0, len - 1) == 1)
-	{
-		free(array);
+	buscar = *head;
+	if (!*head || (*head)->next == NULL)
 		return (1);
+
+	for (con1 = 0; buscar; buscar = buscar->next, con1++)
+		arr[con1] = buscar->n;
+	for (con1--; con1 > con2; con1--, con2++)
+	{
+		if (arr[con2] == arr[con1])
+			;
+		else
+			return (0);
 	}
-	free(array);
-	return (0);
+	return (1);
 }
