2971bb93490573a5fbbcdb55c4aafca004df4e39
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-07 04:15:04 +0200

    Fix requirement error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    try:
-        return ((len(sentence), sentence[0]))
-    except:
-        return((0, None))
+    first = "None" if len(sentence) == 0 else sentence[0]
+    sol = (len(sentence), first)
+    return sol
