ed3edbdee0f2e541452defa79c63e7f9ac20039f
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-07 02:20:31 +0200

    Print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) != 0:
+        for i in matrix:
+            for j in range(len(i)):
+                if j == 0:
+                    print("{:d}".format(i[j]), end='')
+                else:
+                    print(" {:d}".format(i[j]), end='')
+            print()
