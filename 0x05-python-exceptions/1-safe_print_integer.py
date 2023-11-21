e7731d8d8f42d92f2377852401ab05b86ad71bf9
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-21 03:27:39 +0200

    use finally

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        return False
-    else:
         return True
+    except ValueError:
+        return False
+    finally:
+        False
