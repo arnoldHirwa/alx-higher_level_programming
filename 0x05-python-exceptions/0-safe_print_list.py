447d1a1ed45daf972a02f6839e6071c2250ded24
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-21 03:07:45 +0200

    Print safely

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            printed += 1
+        except:
+            printed += 0
+    print()
+    return printed
