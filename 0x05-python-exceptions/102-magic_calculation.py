d85828086aea3f374beec3216cf79fcba6a061bd
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-21 05:01:45 +0200

    fix error

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,7 +9,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += (a ** b / i)
-        except:
+        except Exception:
             result = a + b
             break
 
