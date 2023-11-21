caca4fafd188ae2ec79c824fb6b85717977fe57b
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-21 04:42:52 +0200

    Exception with functions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        sys.stderr.write(f"Exception: {e}\n")
+        return None
