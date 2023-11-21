79ec1e39c37a43050343d82d5e3f8e2ba2a95ba3
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-21 03:10:38 +0200

    fix pycodestyle error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{my_list[i]}", end="")
             printed += 1
-        except:
+        except IndexError:
             printed += 0
     print()
     return printed
