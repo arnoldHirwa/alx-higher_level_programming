c617f85cbcb9145ed2af2f5c08ef6f5e84172a62
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-21 03:15:52 +0200

    Handle valueError exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
-    printed = 0
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return False
 
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}")
-            printed += 1
-        except:
-            printed += 0
-    
-    return printed
+    return True
