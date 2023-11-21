518766b27b4b135e22e432298eb70d19db0a9749
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-21 03:41:13 +0200

    print safe

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+        except ValueError as e:
+            printed += 0
+        except TypeError as t:
+            printed += 0
+    
+    print()
+    return printed
