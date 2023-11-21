9ffc9e6c230a34f29de06af26c74319b145b91bf
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-21 03:53:25 +0200

    ZeroDivisionError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        print("Inside result:", end="")
+        result = a / b
+    except ZeroDivisionError:
+        # print(" None")
+        return None
+    finally:
+        print(" {}".format(result))
+        return result
