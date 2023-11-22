698b7309240dfcd6c36c55931ea39a5a63eddac1
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-22 04:31:21 +0200

    fix errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,11 +8,10 @@ class Square:
     def __init__(self, size=0):
         """Initializes with a size"""
 
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError('size must be an integer')
             """Verify that size is not an integer"""
         if size < 0:
             raise ValueError('size must be >= 0')
             """Verify that size is not negative or 0"""
         self.__size = size
-
