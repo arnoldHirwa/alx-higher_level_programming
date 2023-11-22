fd41a2ed030624b9c891144f7f1aedbf96239719
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-22 05:03:39 +0200

    magic calculations, and square module

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+
+
+"""The square module"""
+
+
+class Square:
+    """The summary line for a class docstring should fit on one line."""
+
+    def __init__(self, size=0):
+        """The summary line for a class docstring should fit on one line."""
+        self.size = size
+
+    def area(self):
+        """The summary line for a class docstring should fit on one line."""
+        return (self._size * self._size)
+
+    @property
+    def size(self):
+        """The summary line for a class docstring should fit on one line."""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """The summary line for a class docstring should fit on one line."""
+        if type(value) == int:
+            if value >= 0:
+                self._size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le___(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Magic Class Module"""
+import math
+
+
+class MagicClass():
+    """ Magic class definitions """
+    def __init__(self, radius=0):
+        self._MagicClass__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self._MagicClass__radius = radius
+
+    def area(self):
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self._MagicClass__radius
+    
