03122ab8ddb033ae078a968bf5e4e5ff1e91a8fb
Hirwa Arnold <arnold.hirw@gmail.com>
2023-12-11 23:57:20 +0200

    fix style errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None or type(roman_string) is not str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     else:
         sum = 0
@@ -42,9 +42,11 @@ def roman_to_int(roman_string):
                 continue
             else:
                 current = symbols[symbol]
-                if idx < len(romArray) - 1 and current["skip"] is romArray[idx + 1]:
+                if idx < len(romArray)-1\
+                   and current["skip"] is romArray[idx + 1]:
+
                     skip = True
                     sum += current["value"] - symbols[current["skip"]]["value"]
                 else:
-                   sum += current["value"] 
+                    sum += current["value"]
         return sum
