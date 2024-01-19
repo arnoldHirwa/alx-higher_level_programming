307a388af6cd787000c4143980039a988dc04623
Hirwa Arnold <arnold.hirw@gmail.com>
2024-01-19 17:52:34 +0200

    magic_string correction

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return ", ".join(l)
+def magic_string():
+    magic_string.static = getattr(magic_string, 'static', -1) + 1
+    return "BestSchool, " * magic_string.static + "BestSchool"
