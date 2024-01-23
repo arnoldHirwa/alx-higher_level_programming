0c11b9d5d521422ee1fac1a9bcec32b5dd66cfab
Hirwa Arnold <arnold.hirw@gmail.com>
2024-01-23 17:57:56 +0200

    Fix errors

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,7 +10,7 @@ valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
 def printInfo():
     """A function for printing information to stdout"""
-    print("Filse size: ", totalSize)
+    print("File size: ", totalSize)
     for key in sorted(codes.keys()):
         print(f"{key}: {codes[key]}")
 
@@ -26,7 +26,6 @@ if __name__ == "__main__":
                 totalSize += int(size)
             except Exception:
                 pass
-            
             try:
                 code = int(splitted[-2])
                 if str(code) in valid_codes:
