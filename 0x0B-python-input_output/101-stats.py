a2f81fadf867e5805dfcf845f8fd98283ce5f889
Hirwa Arnold <arnold.hirw@gmail.com>
2024-01-23 17:55:39 +0200

    new try

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,6 +5,7 @@ import sys
 
 totalSize = 0
 codes = {}
+valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
 
 def printInfo():
@@ -20,13 +21,22 @@ if __name__ == "__main__":
         try:
             data = input()
             splitted = data.split()
-            code = int(splitted[-2])
-            size = splitted[-1]
-            totalSize += int(size)
-            codes[code] = codes.get(code, 0) + 1
+            try:
+                size = splitted[-1]
+                totalSize += int(size)
+            except Exception:
+                pass
+            
+            try:
+                code = int(splitted[-2])
+                if str(code) in valid_codes:
+                    codes[code] = codes.get(code, 0) + 1
+            except Exception:
+                pass
+
             if i % 10 == 0:
                 printInfo()
         except KeyboardInterrupt:
             printInfo()
-            break
+            raise
         i += 1
