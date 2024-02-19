963e380d9f4aa21f1ed1e821d2f941a6d4aeb0aa
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-19 04:19:54 +0200

    safe filter, list cities and states from database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Lists cities of a database """
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
