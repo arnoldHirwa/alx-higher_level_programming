230d522789b74de40301d13edd4b555ff661e5d3
Hirwa Arnold <arnold.hirw@gmail.com>
2024-03-01 04:13:39 +0200

    Initial commit

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Get body for a given URL for 200 status code responses.
+curl -sL "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# DELETE request to a given URL and display the response body.
+curl -sX DELETE "$1"
