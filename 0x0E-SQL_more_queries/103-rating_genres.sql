ebfe2cc0f658edb4b859c21640abe8b7bf487060
Hirwa Arnold <arnold.hirw@gmail.com>
2024-03-21 05:24:05 +0200

    fix case

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,7 @@
 -- list genres in the database hbtn_0d_tvshows_rate by their rating
 -- list rows in a database linked to a row in another table
-SELECT tv_genres.name, sum(tv_show_ratings.rate) as rating FROM tv_genres
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY name
 ORDER BY rating DESC;
-
