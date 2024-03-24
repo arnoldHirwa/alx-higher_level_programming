6cc2f5e36d968d50bfdca061860ad020b15a049d
Hirwa Arnold <arnold.hirw@gmail.com>
2024-03-24 16:29:08 +0200

    Fix: indent error

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,11 +12,12 @@ if __name__ == '__main__':
     name = sys.argv[1]
     paswd = sys.argv[2]
     db = sys.argv[3]
-    engine = create_engine(f'mysql+mysqldb://{name}:{paswd}@localhost:3306/{db}',
-                        pool_pre_ping=True)
+    engine = create_engine(f'mysql+mysqldb://{name}:\
+    {paswd}@localhost:3306/{db}', pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).filter(State.name.like('%a%')).all()
+    states = session.query(State).order_by(State.id)\
+        .filter(State.name.like('%a%')).all()
 
     for st in states:
         print(f'{st.id}: {st.name}')
