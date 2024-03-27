946c2bdbfc0b9e396b45bd1d1211014a8ecbd033
Hirwa Arnold <arnold.hirw@gmail.com>
2024-03-27 05:23:50 +0200

    Fix typo

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,8 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const result = JSON.parse(body).result;
-    console.log(result.reduce((count, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
