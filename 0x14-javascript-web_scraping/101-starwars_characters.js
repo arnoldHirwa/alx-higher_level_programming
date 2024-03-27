f6454cc7aa84335eaaeee0daef450dce042e03c6
Hirwa Arnold <arnold.hirw@gmail.com>
2024-03-27 05:06:27 +0200

    print chars

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printChars(characters, 0);
+  }
+});
+
+function printChars (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printChars(characters, index + 1);
+      }
+    }
+  });
+}
