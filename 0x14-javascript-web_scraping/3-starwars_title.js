3d9aecbc7114398affe9a40e2a8e544c216fa716
Hirwa Arnold <arnold.hirw@gmail.com>
2024-03-27 05:09:19 +0200

    Fix api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
+let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
